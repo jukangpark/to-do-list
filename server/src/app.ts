4110a17f48572329f323b098fdc730bb7e82b337
jukangpark <skyxxx9339@naver.com>
2022-03-15 15:12:58 +0900

    first

---
+++ server/src/app.ts
@@ -0,0 +1,14 @@
+import express from "express";
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.static("build"));
+
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/build/index.html");
+});
+
+app.listen(PORT, () => {
+  console.log("hello world!");
+});
