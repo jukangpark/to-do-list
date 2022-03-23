b7426348621d3e164492efb45a4e4b78e97384c2
jukangpark <skyxxx9339@naver.com>
2022-03-23 23:10:00 +0900

    deploy

--- server/src/models/todos.ts
+++ server/src/models/todos.ts
@@ -14,3 +14,9 @@ export default Todos;
 
 // "To Do": [], Doing: [], Done: [] 로 된 객체여야만 함. Default 로 있는 프로퍼티?
 // 그리고 이 객체는 더 늘어날 수도 있음...
+
+// 스키마 배열 정의는 type: Array 이렇게 (x)
+// [] 이런식?
+
+//  src/controllers/todoController.ts(1,19): error TS2307: Cannot find module '../models/Todos' or its corresponding type declarations.
+// -----> Build failed
